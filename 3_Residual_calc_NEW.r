a674c9cb2b4c00e42b8dda36351390673838ba9d
gmwilliford <91164938+gmwilliford@users.noreply.github.com>
2023-07-20 19:30:57 -0500

    Update 3_Residual_calc_NEW.r

--- 3_Residual_calc_NEW.r
+++ 3_Residual_calc_NEW.r
@@ -1,72 +1,73 @@
 ## RESIDUAL Calculation ##
-
-
-model_data_list <- list()
-
-#loop over each file in the grace_clipped_list and the corresponding file in gldas_stacked_list
-for (i in 1:length(grace_clipped_list)) {
-  #get clipped grace raster
-  grace_clip <- grace_clipped_list[[i]]
-  
-  #get corresponding stack of gldas predictors
-  predictors <- gldas1_stacked[[i]]
-  
-  xy_df <- as.data.frame(grace_clip, xy=TRUE)
-  
-  #combine the predictor variables into a data frame
-  predictors_df <- as.data.frame(predictors, xy=TRUE)
-  
-  #same for response variable
-  response_df <- as.data.frame(grace_clip, xy=TRUE)
-  
-  #subset predictors data frame to exclude x and y columns
-  predictors_df <- predictors_df[, -c(1, 2)]
-  
-  #subset response
-  response_df <- response_df[, -c(1, 2)]
-  
-  model_data <- cbind(predictors_df, response_df)
-  
-  #add the model data to the master list of model data
-  model_data_list[[i]] <- model_data
-}
-
-#define the extent of the new residual raster using minimum and maximum values of longitude and latitude
 xmin <- -94.5
 xmax <- -87.25
 ymin <- 29.75
 ymax <- 37.25
-extent_proj <- extent(xmin, xmax, ymin, ymax)
+extent_resample <- extent(xmin, xmax, ymin, ymax)
+
+
+residuals <- test_data$response_df - predictions
 
 #create empty raster to store residuals
-rast <- raster(extent_proj, res = 0.25, crs = CRS("+proj=longlat +datum=WGS84"))
+rast <- raster(extent_resample, res = 0.25, crs = CRS("+proj=longlat +datum=WGS84"))
+response_df <- do.call(rbind, lapply(grace_clipped_list, function(x) as.data.frame(x, xy = TRUE)))
+response_df$residuals <- 0
+# Assign the calculated residuals to the corresponding rows
+response_df$residuals[test_rows] <- residuals
+
+# Calculate the number of rows in each group (49 rows in your case)
+rows_per_group <- 49
+
+# Calculate the total number of groups
+total_groups <- nrow(response_df) %/% rows_per_group
+
+# Split the dataframe into a list of dataframes, each containing 49 consecutive rows
+split_data <- split(response_df, rep(1:total_groups, each = rows_per_group, length.out = nrow(response_df)))
+
+for (i in seq_along(split_data)) {
+  assign(paste0("grace_nc_", i), split_data[[i]])
+}
+monthly_data_list <- split_data
 
 
+residuals_dataframes <- list()
+response_dataframes <- list()
+
+# Loop over each monthly dataframe
+for (i in seq_along(monthly_data_list)) {
+  # Get the current monthly dataframe
+  monthly_data <- monthly_data_list[[i]]
+  
+  # Extract x and y columns
+  xy_data <- monthly_data[, c("x", "y")]
+  
+  # Extract residuals column and create a new dataframe
+  residuals_df <- data.frame(x = xy_data$x, y = xy_data$y, residuals = monthly_data$residuals)
+  
+  # Extract "Liquid_Water_Equivalent_Thickness" column and create a new dataframe
+  response_df <- data.frame(x = xy_data$x, y = xy_data$y, Liquid_Water_Equivalent_Thickness = monthly_data$Liquid_Water_Equivalent_Thickness)
+  
+  # Add the dataframes to the respective lists
+  residuals_dataframes[[i]] <- residuals_df
+  response_dataframes[[i]] <- response_df
+}
+
+#create empty raster to store residuals
+rast <- raster(extent_resample, res = 0.25, crs = CRS("+proj=longlat +datum=WGS84"))
 
 residuals_rasters <- list()
 
 #loop over each month in the monthly_data_list
-for (i in seq_along(model_data_list)) {
-  #get the monthly data for the current month
-  monthly_data <- model_data_list[[i]]
+for (i in seq_along(residuals_dataframes)) {
   
-  #predict using the model
-  predicted <- predict(model, newdata = monthly_data)
+  # Get the current monthly residuals dataframe
+  residuals_df <- residuals_dataframes[[i]]
   
-  #extract the response variable from the monthly data
-  response_data <- monthly_data[, ncol(monthly_data)]
-  
-  #calculate residuals
-  residuals <- response_data - predicted
-  
-  #create a residual raster with the same extent, resolution, and projection as grace_clip
+  # Create a raster with the same extent, resolution, and projection as grace_clip
   residual_raster <- raster(grace_clip)
   
-  #ensure the values of the residual raster are set to 0
-  values(residual_raster) <- 0
-  
-  #assign the residuals to the corresponding cells in the residual raster
-  residual_raster[] <- residuals
+  # Set the raster values to the residuals using the x and y coordinates
+  residual_raster[] <- residuals_df$residuals
   
   #resample to a 0.25x0.25 grid using bilinear interpolation
   residual_raster_resampled <- resample(residual_raster, rast, method = "bilinear", filename = "")
