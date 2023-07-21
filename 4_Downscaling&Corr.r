6af33e29163da8a8008e245c953a793dbf275ceb
gmwilliford <91164938+gmwilliford@users.noreply.github.com>
2023-07-20 19:30:30 -0500

    Update 4_Downscaling&Corr.r

--- 4_Downscaling&Corr.r
+++ 4_Downscaling&Corr.r
@@ -20,7 +20,7 @@ for (i in 1:length(gldas025_stacked)) {
   predictions_list[[i]] <- predictions
   
   #create an empty raster
-  downscale <- raster(extent_proj, res = 0.25, crs = CRS("+proj=longlat +datum=WGS84"))
+  downscale <- raster(extent_resample, res = 0.25, crs = CRS("+proj=longlat +datum=WGS84"))
   values(downscale) <- predictions
   
   #add the predicted raster to the downscaled list
@@ -33,22 +33,31 @@ corrected <- list()
 
 #loop through each raster in the res_rasters list
 for (i in seq_along(residuals_rasters)) {
-  #get the current raster from res_rasters and downscaled_list
-  res_raster <- residuals_rasters[[i]]
-  downscaled_raster <- downscaled_list[[i]]
+  # Convert residuals and downscaled rasters to data frames
+  resid_df <- as.data.frame(residuals_rasters[[i]], xy = TRUE)
+  downdf <- as.data.frame(downscaled_list[[i]], xy = TRUE)
+
   
-  #add the residual raster to the downscaled raster for residual correction
-  corrected_raster <- res_raster + downscaled_raster
+  # Combine the data frames based on x, y coordinates
+  combined_df <- merge(downdf, resid_df, by = c("x", "y"))
+  
+  # Replace NA values with 0 in the residuals dataframe
+  combined_df[is.na(combined_df)] <- 0
+  
+  # Calculate corrected values by adding residuals to downscaled values
+  combined_df$corrected_value <- combined_df$layer.x + combined_df$layer.y
+  
+  # Remove unnecessary columns and keep only the corrected value
+  correcteddf <- combined_df[, c("x", "y", "corrected_value")]
+  
+  # Convert correcteddf back to a raster
+  corrected_raster <- rasterFromXYZ(correcteddf, res = c(0.25,0.25), CRS("+proj=longlat +datum=WGS84"))
+  
   
   #add the corrected raster to the corrected list
   corrected[[i]] <- corrected_raster
 }
 
-correcteddf <- as.data.frame(corrected[[1]])
-resid_df <- as.data.frame(residuals_rasters[[1]], xy =TRUE)
-downdf <- as.data.frame(downscaled_raster[[1]])
-
-test <- cbind(resid_df, downdf, correcteddf)
 
 ##Calculating GWS##
 
@@ -60,18 +69,28 @@ final_list <- list()
 
 # Loop over each file in the gldas025_stacked list
 for (i in 1:length(gldas025_stacked)) {
-  predictors <- gldas025_stacked[[i]]
   sum_raster <- calc(predictors, sum, na.rm=FALSE, na.rm.data=FALSE, na.rm.interp=FALSE, na.rm.filter=FALSE, forcefun=FALSE, filename="")
-  sum_raster_clipped <- crop(sum_raster, extent_proj)
+  sum_raster_clipped <- crop(sum_raster, extent_resample)
   
   #get the corresponding downscaled raster
-  downscaled_raster <- downscaled_list[[i]]
+  downscaled_raster <- corrected[[i]]
   
   #GWS calculation
-  final_raster <- downscaled_raster*1000 - sum_raster_clipped/1000
+  final_raster <- downscaled_raster*100 - sum_raster_clipped/10
   
   #Accounting for Specific Yield 
   final_raster <- final_raster * 0.25
   
   final_list[[i]] <- final_raster
+}
+
+# Define the new CRS (NAD83, EPSG:4269)
+new_crs <- CRS("+proj=longlat +datum=NAD83 +ellps=GRS80")
+
+# Loop through each raster in the final_list and reproject it to the new CRS
+for (i in seq_along(final_list)) {
+  final_list[[i]] <- projectRaster(final_list[[i]], crs = new_crs)
+  final_df <- as.data.frame(final_list[[i]], xy = TRUE, na.rm = TRUE)
+  final_raster2 <- rasterFromXYZ(final_df)
+  final_list[[i]] <- final_raster2
 }
